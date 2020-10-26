6491a444029b0e40f6810d8c34a0fed7b8e633db
Julian Brea <jbrea@baufest.com>
2020-10-26 20:37:07 -0300

    Variable de enviroment para bdd

--- data/connectionMongo.js
+++ data/connectionMongo.js
@@ -3,7 +3,7 @@ const chalk = require('chalk');
 const { MongoClient } = require('mongodb');
 
 // TODO: Crear variables de entorno, sacar el harcodeo
-const uriMongo = 'mongodb+srv://admin:w6uloovYU0MqNbwi@cluster0.eoafj.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const uriMongo = process.env.MONGOURI;
 
 const client = new MongoClient(uriMongo, {useUnifiedTopology: true, useNewUrlParser:true})
 
